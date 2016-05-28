a16743f1bf542ba8f7c6dacd4a1cbd82a8b45924
lucas <ec03@viajobien.com>
2016-05-28 16:07:34 -0300

    Closes pablofalegre/pdes-tips#23 . .

--- tests/backend/routes/activityLog.test.js
+++ tests/backend/routes/activityLog.test.js
@@ -1,10 +1,23 @@
 var mongoose = require("mongoose");
 var mockgoose = require("mockgoose");
+var proxyquire = require("proxyquire");
 
 require('../../../models/Activities');
-var activityLog = require('../../../routes/activityLog');
+
 
 var httpMock = require("node-mocks-http");
+
+var sinon = require("sinon");
+
+var dummyConfig = {};
+
+var activityLog = proxyquire('../../../routes/activityLog', {
+    '../config/ActivitiesConfig' : dummyConfig
+});
+
+var logger = activityLog.logger;
+var maker = activityLog.maker;
+
 
 describe("router activityLog", function() {
 
@@ -24,20 +37,56 @@ describe("router activityLog", function() {
         });
 
         describe("when intercepting req res", function() {
-            it("should create activity when necessary", function(done) {
+            it("should subscribe a function to 'on' event", function(done) {
                     var req = httpMock.createRequest({
                             method : 'POST',
-                            url : '/ideas'
+                            url : '/ideas',
+                            path : '',
+                            route : {path : ''}
                     });
-                    var res = httpMock.createResponse({
-                              eventEmitter: require('events').EventEmitter
-                    });
-                    activityLog(req, res);
 
-                    res.on('finish', function(){
-                            done();
+                    var res = httpMock.createResponse({
                     });
-                    res.send('');
+
+                    var resSpy = sinon.spy(res, 'on');
+
+                    logger(req, res);
+
+                    sinon.assert.calledOnce(resSpy);
+
+                    done();
+            });
+        });
+
+        describe("when calling makeActivity", function() {
+            it("should create an activity if response was ok and activity could be found", function(done) {
+
+                    var baseUrl = '/ideas';
+
+                    var req = httpMock.createRequest({
+                            method : 'POST',
+                            url : baseUrl,
+                            user : {user : 'lucas'},
+                            path : ''
+                    });
+
+                    req.route = { path : ''};
+                    req.baseUrl = baseUrl;
+
+                    var res = httpMock.createResponse({
+                    });
+
+                    res.statusCode = 200;
+
+                    configSpy = sinon.spy(dummyConfig, 'find');
+
+                    maker(req, res);
+
+                    sinon.assert.calledOnce(configSpy);
+
+                    configSpy.calledWith(baseUrl, 'POST');
+
+                    done();
             });
         });
 });
