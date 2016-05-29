be670d5cc42a854487b3ecbe81f362aa61b67b98
Pablo Alegre <pablofalegre@gmail.com>
2016-05-29 19:30:58 -0300

    Added more gulp tasks for testing an autogenerating dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,84 @@
 // Include gulp
 var gulp = require('gulp');
-// Include plugins
-var mainBowerFiles = require('gulp-main-bower-files');
+// Gulp plugins
+var inject = require('gulp-inject');
 var concat = require('gulp-concat');
+var mainBowerFiles = require('gulp-main-bower-files');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var gulpFilter = require('gulp-filter');
 
-gulp.task('main-bower-files', function() {
-    var filterJS = gulpFilter('**/*.js', { restore: true });
-    return gulp.src('./bower.json')
-        .pipe(mainBowerFiles({
-            overrides: {
-                bootstrap: {
-                    main: [
-                        './dist/js/bootstrap.js',
-                        './dist/css/*.min.*',
-                        './dist/fonts/*.*'
-                    ]
-                }
-            }
-        }))
-        .pipe(filterJS)
-        .pipe(concat('vendor.js'))
-        .pipe(uglify())
-        .pipe(filterJS.restore)
-        .pipe(gulp.dest('public'));
+// Test tools
+var mocha = require("gulp-mocha");
+var KarmaServer = require("karma").Server;
+
+var sources = {
+  js: ["public/javascripts/external/**/*.js", "public/javascripts/angularApp.js", "public/javascripts/modules/services/**/*.js", "public/javascripts/modules/controllers/**/*.js"],
+  test: {
+    frontend: ["tests/frontend/dependencies/**/*.js","public/javascripts/angularApp.js", "public/javascripts/modules/services/**/*.js", "public/javascripts/modules/controllers/**/*.js", "public/javascripts/**/*.js",'tests/frontend/**/*.js'],
+    backend: ["tests/backend/**/*.js"]
+  }
+};
+
+// Test tasks
+gulp.task("test:backend", function() {
+  return gulp.src(sources.test.backend)
+    .pipe(mocha());
 });
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src([
-      'public/javascripts/angularApp.js',
-      'public/javascripts/**/*.js'])
-      .pipe(concat('sources.js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('public'));
+
+gulp.task("dependency:link", ["dependency:external:copy"], function() {
+  var dependencies = gulp.src(sources.js);
+
+  return gulp.src("views/index.ejs")
+    .pipe(inject(dependencies, {
+      ignorePath: 'public',
+      addRootSlash: false
+    }))
+    .pipe(gulp.dest("views"));
 });
-gulp.task('uglify', function(){
-    return gulp.src('./bower.json')
-        .pipe(mainBowerFiles( ))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/sources'));
+
+
+gulp.task("karma:dependency:link", ["dependency:external:copy", "dependency:test:copy"], function() {
+  var dependencies = gulp.src(sources.test.frontend);
+
+  return gulp.src("karma.conf.js")
+    .pipe(inject(dependencies, {
+      starttag: "files: [",
+      endtag: "],",
+      transform: function(path, file, index, total) {
+        return '"' + path + '"' + (index + 1 < total ? "," : "");
+      },
+      addRootSlash: false
+    }))
+    .pipe(gulp.dest("."));
 });
-gulp.task('css', function(){
-  return gulp.src('bower_components/bootstrap/dist/css/bootstrap.css')
-        .pipe(concat('styles.css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('public'));
+
+function dependencyCopy(outDir, outFile, options) {
+  return gulp.src("bower.json")
+      .pipe(mainBowerFiles(options || {}))
+      .pipe(concat(outFile))
+    .pipe(gulp.dest(outDir));
+}
+
+gulp.task("dependency:external:copy", function() {
+  return dependencyCopy("public/javascripts/external", "dependencies.js");
 });
-// Default Task
-gulp.task('default', ['scripts','main-bower-files']);
+
+gulp.task("dependency:test:copy", function() {
+  return dependencyCopy("tests/frontend/dependencies", "test-dependencies.js", {includeDev: true});
+});
+
+gulp.task("test:frontend", ["karma:dependency:link"], function(done) {
+  new KarmaServer({
+    configFile: __dirname + "/karma.conf.js",
+    singleRun: true
+  }).start(done);
+});
+
+
+gulp.task("test:light", ["test:backend", "test:frontend"]);
+
+gulp.task("auto:tests", function() {
+  gulp.watch(["models/**/*.js", "routes/**/*.js"], ["test:backend"]);
+  gulp.watch(["public/modules/**/*.js", "exampleApp.js"], ["test:frontend"]);
+});
