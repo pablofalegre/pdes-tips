4971e209f2b84e9e7d5533b619377eabc5bb4ab7
Lucas <lucas.matw@gmail.com>
2016-05-29 14:15:20 -0300

    23 test activitylog (#28)
    * #15 Version 1 del nav.
    
    * install de mocha y file
    
    * #16 listado de materias. Falta un ABM y que solo pueda verlo el director.
    
    * probando tests
    
    * faltaba js del ActivitiesCtrl en la view.
    
    * #16 listado de materias y poder agregar.
    
    * Closes pablofalegre/pdes-tips#16 . Version 1 de materias.
    
    * #19 no anda login ni register
    
    * #16 fix de register y login
    
    * #19 Falta modificar las views y validar del lado del servidor
    
    * mostrar correctamente el usuario en el html de actividades recientes
    
    * falta terminar
    
    * #19 cambios en la view para ocultar o mostrar segun el rol del usuario
    
    * Closes pablofalegre/pdes-tips#19 . Autorizacion en cliente y servidor.
    
    * Closes pablofalegre/pdes-tips#21 . Links.
    
    * intentando testear
    
    * #23 cambio en el export del activityLog
    
    * Closes pablofalegre/pdes-tips#23 . .

--- routes/activityLog.js
+++ routes/activityLog.js
@@ -4,48 +4,48 @@ var activitiesConf = require('../config/ActivitiesConfig');
 
 var Q = require('q');
 
-var makeActivity = function(req, res){
+var makeActivity = function(req, res) {
     var deferred = Q.defer();
 
     //despues se podria hacer algo para que un middeware deje un usuario siempre (un Anonimo si no estan logeados), y evitamos preguntar.
-    if(res.statusCode == 200 && req.user){
-      
-      //TODO si no se configura el router como corresponde esto no vaa funcionar: el req.route.path va a contener el baseUrl
-      var fixedPath = req.route.path;
+    if (res.statusCode == 200 && req.user) {
 
-      if(fixedPath == "/"){
-        fixedPath = "";
-      }
+        //TODO si no se configura el router como corresponde esto no vaa funcionar: el req.route.path va a contener el baseUrl
+        var fixedPath = req.route.path;
 
-      var activityOpt = activitiesConf.find(req.baseUrl + fixedPath, req.method);
+        if (fixedPath == "/") {
+            fixedPath = "";
+        }
 
-      activityOpt.map(function(activity){
-        
-        activity(req.user, req, res).save(function(err, activity){
-          if(err){
-            console.log('error saving ' + err);
-            //return next(err);
-            deferred.reject(err); 
-          }
-          deferred.resolve(activity);
+        var activityOpt = activitiesConf.find(req.baseUrl + fixedPath, req.method);
+
+        activityOpt.map(function(activity) {
+
+            activity(req.user, req, res).save(function(err, activity) {
+                if (err) {
+                    console.log('error saving ' + err);
+                    //return next(err);
+                    deferred.reject(err);
+                }
+                deferred.resolve(activity);
+            });
+
         });
-        
-      });
     }
-    return deferred.promise;   
-}
+    return deferred.promise;
+};
 
 //Middleware para creacion de eventos.
 var activityLog = function(req, res) {
 
-  var make = function(){
-      makeActivity(req, res).then(function(algo){
-        console.log("activity logged");
-      }).catch(function(err){ 
-        console.log("error logging activity " + err);
-    });
-  }
-  res.on('finish', make);  
+    var make = function() {
+        makeActivity(req, res).then(function(algo) {
+            console.log("activity logged");
+        }).catch(function(err) {
+            console.log("error logging activity " + err);
+        });
+    };
+    res.on('finish', make);
 };
 
-module.exports = activityLog;
+module.exports = { logger: activityLog, maker: makeActivity };
