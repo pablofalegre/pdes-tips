919b50a42f517cdcdb7dbd1e262d80a0052a8737
Lucas <lucas.matw@gmail.com>
2016-07-02 14:34:07 -0300

    Closes pablofalegre/pdes-tips#33 . Poder editar materias. (#34)

--- public/javascripts/modules/controllers/AssignmentCtrl.js
+++ public/javascripts/modules/controllers/AssignmentCtrl.js
@@ -6,23 +6,36 @@ app.controller('AssignmentCtrl', [
 
 		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.assignments = assignments.assignments;
+		$scope.id = '';
 
 		$scope.home = function(){
 			assignments.all();
 		};
-		
 
-		$scope.addAssignment = function(){
-			if(!$scope.name || $scope.name === '') { return; };
-	  		assignments.add({
-	  			name: $scope.name 
-				});
+		$scope.saveAssignment = function(){
+			if(!$scope.name || $scope.name === '') { return; }
+
+			if($scope.id && $scope.id !== ''){
+				assignments.update($scope.idx, {id : $scope.id,
+					name: $scope.name});
+			} else {
+		  		assignments.save({
+		  			name: $scope.name
+					});
+			}
 			$scope.name  = '';
-				
-		}
+			$scope.id = '';
+			$scope.idx = '';
+		};
 
 		$scope.delete = function(assignment){
-			assignments.delete(assignment);	
-		}
+			assignments.delete(assignment);
+		};
+
+		$scope.edit = function(assignment, idx){
+			$scope.name = assignment.name;
+			$scope.id = assignment._id;
+			$scope.idx = idx;
+		};
 	}
 ]);
