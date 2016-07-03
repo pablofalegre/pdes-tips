0f6c0e59f7dcf3092c5397c8d335f3382c7322ae
Pablo Alegre <pablofalegre@gmail.com>
2016-07-03 12:27:21 -0300

    Ran bump-version, also generated a better changelog

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('changelog', function () {
     buffer: false
   })
     .pipe(conventionalChangelog({
-      preset: 'angular' // Or to any other commit message convention you use.
+      preset: '' // Or to any other commit message convention you use.
     }))
     .pipe(gulp.dest('./'));
 });
@@ -112,7 +112,7 @@ gulp.task('github-release', function(done) {
     type: "oauth",
     token: '94e0b142ce645c7e1a378106561df4a1a1dfa14d' // change this to your own GitHub token or use an environment variable
   }, {
-    preset: 'angular' // Or to any other commit message convention you use.
+    preset: 'pdes-tips' // Or to any other commit message convention you use.
   }, done);
 });
 
